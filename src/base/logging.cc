1339063e9a712774101c547732e49721a34dd0c2
sagarkchitnis <sagarc@juniper.net>
2017-07-17 16:42:01 -0700

    added resources for exe - version information

--- src/base/logging.cc
+++ src/base/logging.cc
@@ -13,6 +13,9 @@
 
 #include <boost/format.hpp>
 #include <boost/algorithm/string/predicate.hpp>
+#ifdef _WINDOWS
+  #include "taskutil.h"
+#endif
 
 using namespace log4cplus;
 
@@ -61,9 +64,8 @@ void LoggingInit(const std::string &filename, long maxFileSize, int maxBackupInd
 
     if (useSyslog) {
         helpers::Properties props;
-		std::string syslogident;
-
-		//WINDOWS-TEMP syslogident = 	boost::str( boost::format("%1%[%2%]") % ident % getpid());
+        std::string syslogident;
+        syslogident = 	boost::str( boost::format("%1%[%2%]") % ident % osspecific_getpid());
 
         props.setProperty(LOG4CPLUS_TEXT("facility"),
                           boost::starts_with(syslogFacility, "LOG_")
