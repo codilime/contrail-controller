12e3587b0272e4055096652ea7f91f928b0906f9
Michal Clapinski <michal.clapinski@codilime.com>
2017-07-19 18:28:15 +0200

    JW-885 fixed GetFormattedWindowsErrorMsg issue

--- src/ksync/ksync_sock.cc
+++ src/ksync/ksync_sock.cc
@@ -629,30 +629,6 @@ bool KSyncSock::SendAsyncImpl(IoContext *ioc) {
 	return false;
 }
 
-#ifdef _WINDOWS
-std::string GetFormattedWindowsErrorMsg() {
-    DWORD error = GetLastError();
-    LPSTR message = NULL;
-
-    DWORD flags = (FORMAT_MESSAGE_ALLOCATE_BUFFER |
-                   FORMAT_MESSAGE_FROM_SYSTEM |
-                   FORMAT_MESSAGE_IGNORE_INSERTS);
-    DWORD lang_id = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT);
-    DWORD ret = FormatMessageA(flags, NULL, error, lang_id, (LPSTR)message, 0, NULL);
-
-    std::ostringstream sstr;
-
-    if (ret != 0) {
-        sstr << message << " ";
-    }
-
-    sstr << "[" << error << "]";
-    LocalFree(message);
-
-    return sstr.str();
-}
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // KSyncSockNetlink routines
 /////////////////////////////////////////////////////////////////////////////
