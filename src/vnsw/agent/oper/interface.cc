24c92e4f394ab627b8826729c843da729fe4219f
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2017-09-04 15:27:05 +0200

    JW-1093 Added comments

--- src/vnsw/agent/oper/interface.cc
+++ src/vnsw/agent/oper/interface.cc
@@ -615,6 +615,9 @@ void Interface::GetOsParams(Agent *agent) {
     }
 
 #ifdef _WIN32
+    /* In case of pkt0 interface, we assume that it is UP, set os_index to dummy value 0,
+        since on Windows this parameter is not used because interfaces are represented by named pipes.
+        Name and mac are set to constant values from agent specific for that interface */
     if (type_ == PACKET) {
         os_oper_state_ = true;
         os_index_ = 0;
