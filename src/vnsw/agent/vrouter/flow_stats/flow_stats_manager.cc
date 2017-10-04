0a506a0a855ace2e516f448e5094e6d89e3be34f
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2017-10-04 13:04:40 +0200

    JW-450: fixes for flow stats collection

--- src/vnsw/agent/vrouter/flow_stats/flow_stats_manager.cc
+++ src/vnsw/agent/vrouter/flow_stats/flow_stats_manager.cc
@@ -89,6 +89,11 @@ FlowStatsManager::FlowStatsManager(Agent *agent) : agent_(agent),
     flow_export_drops_ = 0;
     flows_sampled_atleast_once_ = false;
     request_queue_.set_measure_busy_time(agent->MeasureQueueDelay());
+
+    size_t protocol_list_count = sizeof(protocol_list_) / sizeof(protocol_list_[0]);
+    for (int i = 0; i < protocol_list_count; ++i) {
+        protocol_list_[i] = NULL;
+    }
 }
 
 FlowStatsManager::~FlowStatsManager() {
