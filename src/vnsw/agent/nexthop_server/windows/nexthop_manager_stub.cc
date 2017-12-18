65e85e79e5124aae40b3ea1863b239a6ae81eeb7
Pawel Marchewka <pawel.marchewka@codilime.com>
2017-12-18 17:25:55 +0100

    JW-1330 : Added nexthop_manager stub implementation

---
+++ src/vnsw/agent/nexthop_server/windows/nexthop_manager_stub.cc
@@ -0,0 +1,15 @@
+#include <nexthop_server/nexthop_manager.h>
+
+NexthopManager::NexthopManager(EventManager *evm, const std::string &endpoint) {
+}
+
+NexthopManager::~NexthopManager() {
+}
+
+void
+NexthopManager::Initialize(DB *database) {
+}
+
+void
+NexthopManager::Terminate() {
+}
